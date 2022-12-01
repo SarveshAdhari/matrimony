83269e8966f5eaacd77c6c2460b792d70efc20f1
SarveshAdhari <sarveshadhari7@gmail.com>
2022-12-01 18:38:53 +0530

    Pagination on Get Users(Server Side)

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,8 +95,17 @@ const getAllUsers = async (req, res) => {
         }
         let result = User.find(queryObject)
 
+        const page = Number(req.query.page) || 1
+        const limit = Number(req.query.limit) || 10
+        const skip = (page-1)*limit
+        result = result.skip(skip).limit(limit)
+
         const users = await result
-    res.status(StatusCodes.OK).json({users, totalUsers: users.length, pages: 1})
+
+        const totalUsers = await User.countDocuments(queryObject)
+        const pages = Math.ceil(totalUsers/ limit)
+
+        res.status(StatusCodes.OK).json({users, totalUsers, pages})
     } catch (error) {
         throw new UnAuthenticatedError('Not aunthenticated to access this resource...')
     }
